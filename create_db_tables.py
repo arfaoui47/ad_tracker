de5437b4b3fc9cfe5b5fb2913cffa9b61aed9a09
arfaoui47 <arfaoui47@gmail.com>
2016-11-22 20:48:33 +0100

    ADD: create_db_tables file

---
+++ create_db_tables.py
@@ -0,0 +1,39 @@
+from configparser import ConfigParser
+import MySQLdb
+
+
+def db_connection(config):
+	connexion = MySQLdb.connect(host=config.get('MySQL', 'host'),
+	                  		   user=config.get('MySQL', 'user'),
+	                  		   passwd=config.get('MySQL', 'password'),
+	                  		   db=config.get('MySQL', 'db'))
+	
+	return connexion
+
+
+def create_database(config):
+	connexion = MySQLdb.connect(host=config.get('MySQL', 'host'),
+	                  		   user=config.get('MySQL', 'user'),
+	                  		   passwd=config.get('MySQL', 'password'),)
+	cursor = connexion.cursor()
+	cursor.execute('CREATE DATABASE {};'.format(config.get('MySQL', 'db')))
+
+
+
+def create_images_table(connexion):
+	cursor = connexion.cursor()
+	cursor.execute("""CREATE TABLE images (
+		id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		checksum VARCHAR(256),
+		date_creation datetime)"""
+		)
+	
+if __name__ == '__main__':
+	config = ConfigParser()
+	config.read('conf.ini')
+	try:
+		create_database(config)
+	except:
+		pass
+	conn = db_connection(config)
+	create_images_table(conn)
