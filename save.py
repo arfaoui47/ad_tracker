4b45bab65402db475380f0b42974c9f66a1f5d7f
arfaoui47 <arfaoui47@gmail.com>
2016-11-24 22:50:29 +0100

    Update: Refactor functions to find simple iframes dynamically

--- save.py
+++ save.py
@@ -2,111 +2,114 @@ from hashlib import md5
 from upload_to_S3 import upload_to_S3
 import requests
 import MySQLdb
-import string 
+import string
 import random
 import os
 import time
 from configparser import ConfigParser
 
-
 config = ConfigParser()
 config.read('conf.ini')
 
+
 def file_name_generator(size=6, chars=string.ascii_uppercase + string.digits):
-	return ''.join(random.choice(chars) for _ in range(size))
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 def db_connection():
-	connexion = MySQLdb.connect(host=config.get('MySQL', 'host'),
-                  		   user=config.get('MySQL', 'user'),
-                  		   passwd=config.get('MySQL', 'password'),
-                  		   db=config.get('MySQL', 'db'))
-	
-	return connexion
+    connexion = MySQLdb.connect(host=config.get('MySQL', 'host'),
+                                user=config.get('MySQL', 'user'),
+                                passwd=config.get('MySQL', 'password'),
+                                db=config.get('MySQL', 'db'))
+
+    return connexion
 
 
 def data_retrieve(data, connexion):
-	cursor = connexion.cursor()
-	cursor.execute("select * from images where checksum={}".format(`data`))
-	result = cursor.fetchall()
-	print '[+] retrieving data ...'
-	return True if len(result)==0 else False
+    cursor = connexion.cursor()
+    cursor.execute("select * from images where checksum={}".format(`data`))
+    result = cursor.fetchall()
+    print '[+] retrieving data ...'
+    return True if len(result) == 0 else False
+
 
 def image_local_save(url):
-	print '[+] Url to save: ', url
-	extension = 'gif'
-	if url[-3:]=='jpg':
-		extension='jpg'
-	elif url[-4:]=='jpeg':
-		extension='jpeg'
-	elif url[-3:]=='png':
-		extension='png'
-	name = file_name_generator()
-	file_name = 'local_images/' + name + '.' + extension
-	with open(file_name,'wb') as f:
-		MAX_TRY = 6
-		try_num = 1
-		img_content = ''
-		while try_num <= MAX_TRY and not img_content:
-			try: 
-				img_content = requests.get(url).content
-			except:
-				time.sleep(3)
-				try_num += 1
-				print '[-] Retreiving gifs, Retry number: ', try_num
+    print '[+] Url to save: ', url
+    extension = 'gif'
+    if url[-3:] == 'jpg':
+        extension = 'jpg'
+    elif url[-4:] == 'jpeg':
+        extension = 'jpeg'
+    elif url[-3:] == 'png':
+        extension = 'png'
+    name = file_name_generator()
+    file_name = 'local_images/' + name + '.' + extension
+    with open(file_name, 'wb') as f:
+        MAX_TRY = 6
+        try_num = 1
+        img_content = ''
+        while try_num <= MAX_TRY and not img_content:
+            try:
+                img_content = requests.get(url).content
+            except:
+                time.sleep(3)
+                try_num += 1
+                print '[-] Retreiving gifs, Retry number: ', try_num
 
-		if img_content:
-			f.write(img_content)
-		else:
-			print '[-] Enable to save gif'
-			return 0
-	print '[+] Saving Gif to ', file_name
-	f.close()
-	return file_name
+        if img_content:
+            f.write(img_content)
+        else:
+            print '[-] Enable to save gif'
+            return 0
+    print '[+] Saving Gif to ', file_name
+    f.close()
+    return file_name
+
 
 def data_insert(data, connexion):
-	cursor = connexion.cursor()
-	try:
-		cursor.execute("INSERT INTO images(checksum, date_creation) VALUES ({},{})".format(
-			`data`, `time.strftime('%Y-%m-%d %H:%M:%S')`))
-		print '[+] Saving to MySQL database ...'
-		connexion.commit()
-	except:
-		print '[-] Fail to save to DB !'
-		connexion.rollback()
+    cursor = connexion.cursor()
+    try:
+        cursor.execute("INSERT INTO images(checksum, date_creation) VALUES ({},{})".format(
+            `data`, `time.strftime('%Y-%m-%d %H:%M:%S')`))
+        print '[+] Saving to MySQL database ...'
+        connexion.commit()
+    except:
+        print '[-] Fail to save to DB !'
+        connexion.rollback()
+
 
 def to_md5(file):
-	f=open(file, 'rb').read()
-	check_sum = md5(f).hexdigest()
-	print '[+] hashing file to md5: ', check_sum
-	return check_sum
-	
+    f = open(file, 'rb').read()
+    check_sum = md5(f).hexdigest()
+    print '[+] hashing file to md5: ', check_sum
+    return check_sum
+
 
 def get_all_images(urls):
-	pass
-	
+    pass
 
 
 def save_new_gifs(urls):
-	conn = db_connection()
-	for url in urls:
-		# if 'tpc' in url:
-		file = image_local_save(url);
-		if file:
-			md5_hash = to_md5(file)
-			upload_to_S3(md5_hash, file)
-			try:
-				os.remove(file)
-			except:
-				pass
-			if data_retrieve(md5_hash, conn):
-				data_insert(md5_hash, conn)
-			else:
-				print '[-] Data is already in database'
-			print
+    conn = db_connection()
+    for url in urls:
+        # if 'tpc' in url:
+        file = image_local_save(url);
+        if file:
+            md5_hash = to_md5(file)
+            upload_to_S3(md5_hash, file)
+            # try:																		#TODO
+            # 	os.remove(file)
+            # except:
+            # 	pass
+            if data_retrieve(md5_hash, conn):
+                data_insert(md5_hash, conn)
+            else:
+                print '[-] Data is already in database'
+            print
 
 
 if __name__ == '__main__':
-	#urls = ['https://tpc.googlesyndication.com/simgad/1751857741877065419']
-	# urls = ['https://tpc.googlesyndication.com/simgad/8537187466804554545']
-	urls = ['http://ds.serving-sys.com/BurstingRes///Site-83489/Type-0/768420c0-8760-431a-ad87-3b79c160c867.jpg']
-	save_new_gifs(urls)
+    # urls = ['https://tpc.googlesyndication.com/simgad/1751857741877065419']
+    # urls = ['https://tpc.googlesyndication.com/simgad/8537187466804554545']
+    urls = ['http://ds.serving-sys.com/BurstingRes///Site-83489/Type-0/768420c0-8760-431a-ad87-3b79c160c867.jpg']
+    save_new_gifs(urls)
--- tor_webdriver.py
+++ tor_webdriver.py
@@ -1,12 +1,13 @@
 from pyvirtualdisplay import Display
-#Import webdriver from the selenium library
+# Import webdriver from the selenium library
 from selenium import webdriver
 
-#Import the Proxy class from the selenium library
+# Import the Proxy class from the selenium library
 from selenium.webdriver.common.proxy import *
 
-#Build the Proxy object using the locally running Privoxy
+# Build the Proxy object using the locally running Privoxy
 import random
+
 
 def LoadUserAgents(uafile):
     """
@@ -17,18 +18,19 @@ def LoadUserAgents(uafile):
     with open(uafile, 'rb') as uaf:
         for ua in uaf.readlines():
             if ua:
-                uas.append(ua.strip()[1:-1-1])
+                uas.append(ua.strip()[1:-1 - 1])
     random.shuffle(uas)
     return uas
 
-sizes = [(750,1334), (800,600), (1080,1920), (640,1136),(640,609),(320,480),(1440,2560),(768,1280),
-	(540,960),(480,800),(720,1280),(720,720),(360,480),(480,720),(768,1024),(1536,2048),(480,800),
-	(600, 1024),(540,960),(200,400),(1200,1920)]
+
+sizes = [(750, 1334), (800, 600), (1080, 1920), (640, 1136), (640, 609), (320, 480), (1440, 2560), (768, 1280),
+         (540, 960), (480, 800), (720, 1280), (720, 720), (360, 480), (480, 720), (768, 1024), (1536, 2048), (480, 800),
+         (600, 1024), (540, 960), (200, 400), (1200, 1920)]
 
 uas = LoadUserAgents("user_agents.txt")
 
-port = "8118" #The Privoxy (HTTP) port
-myProxy = "127.0.0.1:"+port
+port = "8118"  # The Privoxy (HTTP) port
+myProxy = "127.0.0.1:" + port
 proxy = Proxy({
     'proxyType': ProxyType.MANUAL,
     'httpProxy': myProxy,
@@ -37,23 +39,31 @@ proxy = Proxy({
     'noProxy': ''
 })
 
-def tor_driver():	
-	User_agent = random.choice(uas)
-	resolution = random.choice(sizes)
-	print User_agent
-	print resolution
-	display = Display(visible=1, size=resolution)
-	display.start()
-	profile = webdriver.FirefoxProfile()
-	profile.set_preference("general.useragent.override", User_agent)
-	driver = webdriver.Firefox(profile, executable_path='./geckodriver', proxy=proxy)
-	driver.set_window_size(*resolution)
-	# return webdriver.Firefox(proxy=proxy), display
-	return driver, display
+
+class MockDisplay():
+    def stop(self):
+        return 1
+
+
+def tor_driver():
+    # User_agent = random.choice(uas)
+    # resolution = random.choice(sizes)
+    # print User_agent
+    # print resolution
+    # display = Display(visible=1, size=resolution)
+    # display.start()
+    # profile = webdriver.FirefoxProfile()
+    # profile.set_preference("general.useragent.override", User_agent)
+    # driver = webdriver.Firefox(profile, executable_path='./geckodriver', proxy=proxy)
+    # driver.set_window_size(*resolution)
+    return webdriver.Firefox(executable_path='./geckodriver'), MockDisplay()
+
+
+# return driver, display
 
 
 if __name__ == '__main__':
-	driver = tor_driver()
-	driver.get("http://www.ipligence.com/geolocation")
-	driver = tor_driver()
-	driver.get("https://check.torproject.org/")	
+    driver = tor_driver()
+    driver.get("http://www.ipligence.com/geolocation")
+    driver = tor_driver()
+    driver.get("https://check.torproject.org/")
